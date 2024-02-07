e59568b6deb4033b19888408eb6f0896e2006543
rhoover <robin.hoover@gmail.com>
2024-02-06 22:09:03 -0500

    Fix Typo

--- .eleventy.js
+++ .eleventy.js
@@ -48,7 +48,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksAsyncFilter("jsmin", jsMinifier);
   
   ////////////////////////////////////////////////////
-  // Utililities
+  // Utilities
   ////////////////////////////////////////////////////
 
   // minify html for production build
