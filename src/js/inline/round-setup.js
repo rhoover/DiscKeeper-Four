8422beb0963646dec08ba9a3ade9d9aa8873dec5
rhoover <robin.hoover@gmail.com>
2024-03-16 15:35:39 -0400

    Modal Form

--- src/js/inline/round-setup.js
+++ src/js/inline/round-setup.js
@@ -43,11 +43,17 @@
       let coursesOutput = "";
 
       courseList.forEach(function(course) {
+        // coursesOutput += `
+        // <p class="modal-courses-item" data-courseID="${course.courseID}">${course.courseName}
+        //   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="modal-courses-item-checkmark"><path d="M170.718 216.482L141.6 245.6l93.6 93.6 208-208-29.118-29.118L235.2 279.918l-64.482-63.436zM422.4 256c0 91.518-74.883 166.4-166.4 166.4S89.6 347.518 89.6 256 164.482 89.6 256 89.6c15.6 0 31.2 2.082 45.764 6.241L334 63.6C310.082 53.2 284.082 48 256 48 141.6 48 48 141.6 48 256s93.6 208 208 208 208-93.6 208-208h-41.6z"/></svg>
+        // </p>
+        // `;
         coursesOutput += `
-        <p class="modal-courses-item" data-courseID="${course.courseID}">${course.courseName}
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="modal-courses-item-checkmark"><path d="M170.718 216.482L141.6 245.6l93.6 93.6 208-208-29.118-29.118L235.2 279.918l-64.482-63.436zM422.4 256c0 91.518-74.883 166.4-166.4 166.4S89.6 347.518 89.6 256 164.482 89.6 256 89.6c15.6 0 31.2 2.082 45.764 6.241L334 63.6C310.082 53.2 284.082 48 256 48 141.6 48 48 141.6 48 256s93.6 208 208 208 208-93.6 208-208h-41.6z"/></svg>
-        </p>
-        `;
+        <label class="modal-courses-item">
+          ${course.courseName}
+          <input type="checkbox" data-courseID="${course.courseID}" />
+        </label>
+        `
       }); // end for loop
 
       insertCoursesHere.innerHTML = coursesOutput;
@@ -138,7 +144,7 @@
 
       playerList.forEach((player) => {
         playersOutput += `
-        <p class="modal-players-item" data-playerID="${player.playerID}">${player.nameFirst}
+        <p class="modal-players-item" data-playerID="${player.playerID}">${player.nameFirst} ${player.nameLast}
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="modal-players-item-checkmark"><path d="M170.718 216.482L141.6 245.6l93.6 93.6 208-208-29.118-29.118L235.2 279.918l-64.482-63.436zM422.4 256c0 91.518-74.883 166.4-166.4 166.4S89.6 347.518 89.6 256 164.482 89.6 256 89.6c15.6 0 31.2 2.082 45.764 6.241L334 63.6C310.082 53.2 284.082 48 256 48 141.6 48 48 141.6 48 256s93.6 208 208 208 208-93.6 208-208h-41.6z"/></svg>
         </p>
         `;
