e347b12998ff65f85156024c927db74713271e5e
rhoover <robin.hoover@gmail.com>
2024-03-30 14:40:23 -0400

    Cleaner

--- src/_eleventy/utilities/cacheName.js
+++ src/_eleventy/utilities/cacheName.js
@@ -6,8 +6,10 @@ let newName = () => {
   const charactersLength = characters.length;
   for ( let i = 0; i < 7; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+  };
+
   result.toString();
+  
   let cacheName = `disckeeperCache-${result}`;
   return cacheName
 };
