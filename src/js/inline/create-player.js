ddd5231cbd8be7502ccea07d652b0c2f86b19acd
rhoover <robin.hoover@gmail.com>
2024-04-05 14:47:09 -0400

    First Dialog Modal

--- src/js/inline/create-player.js
+++ src/js/inline/create-player.js
@@ -65,15 +65,40 @@
 
     storePlayer(finishedList) {
       let success = document.querySelector('.createplayer-success');
+      let formElement = document.querySelector('.createplayer-form');
 
       localforage.setItem('playerList', finishedList);
       
       //some UI assistance
-      success.classList.add('createplayer-success-display');
-
+      // success.classList.add('createplayer-success-display');
       setTimeout(() => {
-        window.location.href = '/';
-      }, 1000);
+        success.showModal();
+      }, 500);
+
+      // https://www.linkedin.com/pulse/how-make-modals-html-dialogtag-vanilla-react-mike-cronin-p2loe
+      const handleBackdropClick = (e) => {
+        if (!e.target.matches('dialog')) return;
+        const { top, bottom, left, right } = e.target.getBoundingClientRect();
+        const { clientX: mouseX, clientY: mouseY } = e;
+      
+        // Ignore radio button arrow movement "clicks"
+        // https://github.com/facebook/react/issues/7407
+        if (mouseX === 0 && mouseY === 0) return;
+      
+        const clickedOutsideOfModalBox = (
+          mouseX <= left || mouseX >= right ||
+          mouseY <= top || mouseY >= bottom
+        );
+      
+        if (clickedOutsideOfModalBox) success.close();
+        formElement.reset();
+      }
+      
+      success.addEventListener('click', handleBackdropClick); 
+
+      // setTimeout(() => {
+        // window.location.href = '/';
+      // }, 1000);
     } // end storePlayer()
   };
   createNewPlayer.init();
